54d4099c57c6127758e550aebe2a9afcc49f5ff6
ayushmakafle <amikafle1@gmail.com>
2023-10-17 17:57:40 +0545

    setup

--- ecom/frontend/src/App.js
+++ ecom/frontend/src/App.js
@@ -1,28 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-decoration: underline;">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className="text-3xl font-bold underline text-black">
-    Hello world!
-  </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      app
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
