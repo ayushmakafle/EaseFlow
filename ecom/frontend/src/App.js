90a1deef641265f90b180951bb95474328a3d6af
ayushmakafle <amikafle1@gmail.com>
2023-10-18 19:00:59 +0545

    login

--- ecom/frontend/src/App.js
+++ ecom/frontend/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import "./App.css";
+import {BrowserRouter, Routes,Route} from "react-router-dom"; 
+import {LoginPage} from "./Routes.js";
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
