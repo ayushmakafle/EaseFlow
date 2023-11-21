494dcf590b77079695f2578925425427709a4937
ayushmakafle <amikafle1@gmail.com>
2023-11-21 23:19:31 +0545

    ecom

--- ecom/frontend/src/Routes.js
+++
@@ -1,7 +0,0 @@
-import LoginPage from "../src/pages/Login";
-import SignUpPage from '../src/pages/SignUpPage';
-import GynecologistLoginPage from '../src/pages/GynecologistLoginPage';
-import GynecologistSignUpPage from '../src/pages/GynecologistSignUpPage.jsx';
-export {
-    LoginPage, SignUpPage, GynecologistLoginPage, GynecologistSignUpPage
-};
---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {Container} from 'react-bootstrap';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <>
+    <main>
+    <Container>
+      <h1>Ecom</h1>
+    </Container>
+    </main>
+    
+    <Footer />
+    </>
+  );
+}
+
+export default App;
