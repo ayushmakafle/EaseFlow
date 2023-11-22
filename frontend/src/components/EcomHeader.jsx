6b5255d8075a6e9f2d80a81edf33c943887a35cf
ayushmakafle <amikafle1@gmail.com>
2023-11-22 19:04:55 +0545

    updated ecomheader

--- frontend/src/components/EcomHeader.jsx
+++ frontend/src/components/EcomHeader.jsx
@@ -1,33 +1,27 @@
 import React from 'react';
 import { Nav, Navbar, Container, Form, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import {LinkContainer} from 'react-router-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const EcomHeader = () => {
   return (
     <>
       <Navbar expand="lg" bg="primary" collapseOnSelect>
-        <Container>
+        <Container className="justify-content-between">
           <LinkContainer to='/'>
             <Navbar.Brand>Products</Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className='ml-auto'>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="danger">Search</Button>
-              </Form>
-            <LinkContainer to='/whyshophere'>
-              <Navbar.Brand>Why Shop at EaseFlow?</Navbar.Brand>
-            </LinkContainer>
-          </Nav>
-          </Navbar.Collapse>
+          <Form className="d-flex mx-auto">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="danger">Search</Button>
+          </Form>
+          <LinkContainer to='/whyshophere'>
+            <Nav.Link>Why Shop at EaseFlow?</Nav.Link>
+          </LinkContainer>
         </Container>
       </Navbar>
     </>
